20bf5673e9a4e3e828499bf2e546ec49bed1ccda
patriciagksmith <30450932+patriciagksmith@users.noreply.github.com>
2018-03-04 11:53:24 -0500

    Update burgersController.js

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -17,20 +17,17 @@ router.get("/burgers", function(req, res) {
 
 
 router.post("/burgers/create", function(req, res) {
-
   burger.create(req.body.burger_name, function(result) {
-
     console.log(result);
     res.redirect("/");
   });
 });
 
-
-router.put("/burgers/update", function(req, res) {
-  burger.update(req.body.burger_id, function(result) {
+router.put("/burgers/:id", function(req, res) {
+  burger.update(req.params.id, function(result) {
 
     console.log(result);
-    res.redirect("/");
+    res.sendStatus(200);
   });
 });
 
