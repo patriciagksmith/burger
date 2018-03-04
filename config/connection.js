a86b6edb21a60f9f6b6c7e75f6cbc7b0b2950dfe
patriciagksmith <30450932+patriciagksmith@users.noreply.github.com>
2018-03-04 11:51:51 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -9,10 +9,17 @@ var connection;
 connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Webdsnr62",
+  password: "$Webdsnr62",
   database: "burgers_db"
   });
 };
 // Make connection.
-connection.connect();
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
 module.exports = connection;
